369a614c8a68671458a178b33b2f7c0f3a92b398
Afsl Thaj <afsal.taj06@gmail.com>
2024-08-21 16:16:06 +1000

    Make sure a running verison

--- definitions-and-scripts/try_out.sh
+++ definitions-and-scripts/try_out.sh
@@ -7,3 +7,4 @@ curl -H "Accept: application/json" -X GET http://localhost:9006/v3/latest-event-
 curl -H "Accept: application/json" -X GET http://localhost:9006/v3/status/100?device-type=ios
 curl -H "Accept: application/json" -X GET http://localhost:9006/v3/player-state/100?device-type=ios
 curl -H "Accept: application/json" -X GET http://localhost:9006/v3/total-play-time/100?device-type=ios
+curl -H "Accept: application/json" -X GET http://localhost:9006/v3/total-play-time-of-movie/100?device-type=ios&movie-name=matrix
