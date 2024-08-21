a507e824aed8b38baf5cbffc61ab2f606174d102
Afsl Thaj <afsal.taj06@gmail.com>
2024-08-21 23:04:01 +1000

    Add variant case

--- definitions-and-scripts/set_up.sh
+++ definitions-and-scripts/set_up.sh
@@ -1,15 +1,3 @@
-# To be executed one by one
-golem-cli api-definition add api-definition.json
-golem-cli api-deployment deploy --definition video-analytics/0.0.1 --host localhost:9006
-golem-cli api-definition list
-
-
-# To be executed one by one
-golem-cli api-definition add api-definition-v2.json
-golem-cli api-deployment deploy --definition video-analytics/0.0.2 --host localhost:9006
-golem-cli api-definition list
-
-# To be executed one by one
 golem-cli api-definition add api-definition-v3.json
 golem-cli api-deployment deploy --definition video-analytics/0.0.3 --host localhost:9006
 golem-cli api-definition list
--- definitions-and-scripts/try_out.sh
+++ definitions-and-scripts/try_out.sh
@@ -11,5 +11,6 @@ curl -H "Accept: application/json" -X GET http://localhost:9006/v3/total-play-ti
 curl -H "Accept: application/json" -X GET http://localhost:9006/v3/total-play-time/100?device-type=invalid
 curl -H "Accept: application/json" -X GET http://localhost:9006/v3/total-play-time-of-movie/100?device-type=ios&movie-name=matrix
 curl -H "Accept: application/json" -X GET http://localhost:9006/v3/total-play-time-of-movie/100?device-type=ioss&movie-name=brahma
+curl -H "Accept: application/json" -X GET http://localhost:9006/v3/get-last-event-timestamp-by-type/100?event-type=buffer
 curl -H "Accept: application/json" -X POST http://localhost:9006/v3/add-event -d @request-body.json
 curl -H "Accept: application/json" -X POST http://localhost:9006/v3/add-event -d @invalid-request-body.json
