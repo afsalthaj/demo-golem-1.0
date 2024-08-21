f0097d00bc091b97837a1da0df9f846c44007aee
Afsl Thaj <afsal.taj06@gmail.com>
2024-08-21 17:01:54 +1000

    Add all possibilites of rib

--- definitions-and-scripts/try_out.sh
+++ definitions-and-scripts/try_out.sh
@@ -7,4 +7,9 @@ curl -H "Accept: application/json" -X GET http://localhost:9006/v3/latest-event-
 curl -H "Accept: application/json" -X GET http://localhost:9006/v3/status/100?device-type=ios
 curl -H "Accept: application/json" -X GET http://localhost:9006/v3/player-state/100?device-type=ios
 curl -H "Accept: application/json" -X GET http://localhost:9006/v3/total-play-time/100?device-type=ios
+curl -H "Accept: application/json" -X GET http://localhost:9006/v3/total-play-time/100?device-type=android
+curl -H "Accept: application/json" -X GET http://localhost:9006/v3/total-play-time/100?device-type=invalid
 curl -H "Accept: application/json" -X GET http://localhost:9006/v3/total-play-time-of-movie/100?device-type=ios&movie-name=matrix
+curl -H "Accept: application/json" -X GET http://localhost:9006/v3/total-play-time-of-movie/100?device-type=ioss&movie-name=brahma
+curl -H "Accept: application/json" -X POST http://localhost:9006/v3/add-event -d @request-body.json
+curl -H "Accept: application/json" -X POST http://localhost:9006/v3/add-event -d @invalid-request-body.json
