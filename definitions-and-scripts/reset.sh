0ff8383af683f76f7e4a3ff058bf8014f48b02cc
Afsl Thaj <afsal.taj06@gmail.com>
2024-08-21 16:02:55 +1000

    Device type not supported

--- definitions-and-scripts/reset.sh
+++ definitions-and-scripts/reset.sh
@@ -1,3 +1,4 @@
  golem-cli api-deployment delete localhost:9006
  golem-cli api-definition delete --id video-analytics --version  0.0.1
  golem-cli api-definition delete --id video-analytics --version  0.0.2
+ golem-cli api-definition delete --id video-analytics --version  0.0.3
--- definitions-and-scripts/try_out.sh
+++ definitions-and-scripts/try_out.sh
@@ -6,3 +6,4 @@ curl -H "Accept: application/json" -X GET http://localhost:9006/v2/player-state/
 curl -H "Accept: application/json" -X GET http://localhost:9006/v3/latest-event-timestamp/buffer/100
 curl -H "Accept: application/json" -X GET http://localhost:9006/v3/status/100?device-type=ios
 curl -H "Accept: application/json" -X GET http://localhost:9006/v3/player-state/100?device-type=ios
+curl -H "Accept: application/json" -X GET http://localhost:9006/v3/total-play-time/100?device-type=ios
